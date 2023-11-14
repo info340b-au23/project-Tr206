9d184bfa9b7419016275eca922a7a433d48f64c4
Morgan Weber <morgweb@uw.edu>
2023-11-14 15:55:28 -0800

    fixed icon issues

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,22 +3,50 @@ import { Navigation } from './Navigation';
 import { Profile } from './Profile';
 
 export function MainPage() {
+  // Header component
   function Header() {
-    return <header>Header content</header>;
+    return (
+      <header>
+        <div className="container">
+          <h1>Diabetes Health Checker</h1>
+        </div>
+      </header>
+    );
   }
 
+  // Footer component
   function Footer() {
-    return <footer>Footer content</footer>;
+    return (
+      <footer className="fixed-bottom">
+        <div className="container">
+          <p><a href="mailto:healthchecker@gmail.com"><span className="material-icons">email</span>healthchecker@gmail.com</a></p>
+          <p><a href="tel:555-123-4567"><span className="material-icons">phone</span> 555-123-4567</a></p>
+          <p>&copy; Health Symptom Checker 2023</p>
+        </div>
+      </footer>
+    );
   }
 
+  // Card component
   function Card(props) {
     return (
-      <div className="card">
-        {/* Card content */}
-        <img src={props.imgSrc} alt={props.title} />
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        <button>{props.buttonText}</button>
+      <div className="d-flex col-md-6 col-xl-3 mb-4">
+        <div className="card h-100 w-100 mb-4">
+          <div className="card-body d-flex flex-column">
+            <div className="row g-0">
+              <div className="col-sm-auto col-xl-12">
+                <img src={props.imgSrc} alt={props.title} className="pb-3 card-image" />
+              </div>
+              <div className="col-sm">
+                <h2 className="card-title">{props.title}</h2>
+                <p className="card-text">{props.description}</p>
+                {props.buttonText && (
+                  <a href={props.buttonLink} className="btn btn-dark mt-auto">{props.buttonText}</a>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -49,6 +77,7 @@ export function MainPage() {
           imgSrc="img/hospital.jpg"
           title="Find a treatment center near you"
           description="Want to follow up with a doctor find a hospital near you?"
+          buttonLink="hospital.html"
           buttonText="Find your hospital"
         />
       </div>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,27 +8,6 @@ export function Profile() {
             <h1>User Profile</h1>
           </div>
         </header>
-  
-        <div className="container">
-          {/* Add profile content here */}
-          <p>User information goes here...</p>
-        </div>
-  
-        <footer className="fixed-bottom">
-          <div className="container">
-            <p>
-              <a href="mailto:user@example.com">
-                <span className="material-icons">email</span>user@example.com
-              </a>
-            </p>
-            <p>
-              <a href="tel:555-555-5555">
-                <span className="material-icons">phone</span>555-555-5555
-              </a>
-            </p>
-            <p>&copy; User Profile 2023</p>
-          </div>
-        </footer>
       </div>
     );
 }
