e914bbba7422bd53f0381c0acd4c8a625bb58033
Sanika Gadam <sanikagadam@Sanikas-MacBook-Pro.local>
2023-11-20 11:48:24 -0800

    Pushing changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { MainPage } from './MainPage';
 import { Profile } from './Profile';
+import { DiaryPage } from './DiaryPage';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/DiaryPage" element={<DiaryPage/>}/>
         {/* Add more routes as needed */}
       </Routes>
     </Router>
