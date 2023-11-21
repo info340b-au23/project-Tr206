638facfa6c53378718816d6381e7e2fc8a6ff1f0
Sanika Gadam <sanikagadam@Sanikas-MacBook-Pro.local>
2023-11-20 19:13:21 -0800

    fixing app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { MainPage } from './MainPage';
-import { Profile } from './Profile';
-import { DiaryPage } from './DiaryPage';
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainPage } from './MainPage';
 import { Profile } from './Profile';
@@ -14,7 +6,6 @@ import { Diary } from './Diary';
 import { FindLocation } from './FindLocation';
 import { HealthStats } from './HealthStats';
 import BloodGlucoseChart from './BloodGlucoseChart';
->>>>>>> 7de61c1cdc3b1d4b1930c32a5ca819086428140d
 
 function App() {
   return (
@@ -22,14 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/profile" element={<Profile />} />
-<<<<<<< HEAD
-        <Route path="/DiaryPage" element={<DiaryPage/>}/>
-=======
         <Route path="/diary" element={<Diary />} />
         <Route path="/findlocation" element={<FindLocation />} /> {/* Route for FindLocation */}
         <Route path="/healthstats" element={<HealthStats />} /> {/* Route for HealthStats */}
-        <Route path="/bloodglucosechart" element={<BloodGlucoseChart />} /> {/* Route for HealthStats */}
->>>>>>> 7de61c1cdc3b1d4b1930c32a5ca819086428140d
+        <Route path="/bloodglucosechart" element={<BloodGlucoseChart />} /> Route for HealthStats
         {/* Add more routes as needed */}
       </Routes>
     </Router>
