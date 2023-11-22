11bfef74c1a1618a6c789827e998f494874528f6
Morgan Weber <morgweb@uw.edu>
2023-11-21 19:31:57 -0800

    fix nav bar when on diary page

--- src/components/DiaryPage.js
+++ src/components/DiaryPage.js
@@ -36,10 +36,11 @@ export function DiaryPage() {
     return (
       <nav>
         <ul>
-          <li><a href="symptom-1.html"><img src="img/quiz.png" alt="Quiz Icon"/>Symptom Quiz</a></li>
-          <li><a href="hospital.html"><img src="img/location.png" alt="Location Icon"/>Find a Treatment Center Near You</a></li>
-          <li><a href="#diary" className="active"><img src="img/diary.png" alt="Diary Icon"/>Diary</a></li>
-          <li><a href="#profile"><img src="img/profile.png" alt="Profile Icon"/>Profile</a></li>
+        <li><Link to="/"><img src="img/home.png" alt="Home Icon" />Home</Link></li>
+        <li><Link to="/HealthStats"><img src="img/quiz.png" alt="Quiz Icon" />Health Tracker</Link></li>
+        <li><Link to="/FindLocation"><img src="img/location.png" alt="Location Icon" />Find a Location</Link></li>
+        <li><Link to="/DiaryPage"><img src="img/diary.png" alt="Diary Icon" />Diary</Link></li>
+        <li><Link to="/Profile"><img src="img/profile.png" alt="Profile Icon" />Profile</Link></li>
         </ul>
       </nav>
     );
