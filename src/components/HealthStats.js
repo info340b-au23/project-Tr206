f98ce3427c6d910498d81f3c7918e726410471b2
qiulinqu <qiulinqu@uw.edu>
2023-12-06 19:48:13 -0800

    css and html

--- src/components/HealthStats.js
+++ src/components/HealthStats.js
@@ -3,6 +3,28 @@ import { useLocation } from 'react-router-dom';
 import { Navigation } from './Navigation';
 import BloodGlucoseChart from './BloodGlucoseChart';
 
+function Header() {
+  return (
+    <header>
+      <div className='container'>
+        <h1>Your Health Statistics</h1>
+      </div>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="fixed-bottom">
+      <div className="container">
+        <p><a href="mailto:healthchecker@gmail.com"><span className="material-icons">email</span>healthchecker@gmail.com</a></p>
+        <p><a href="tel:555-123-4567"><span className="material-icons">phone</span> 555-123-4567</a></p>
+        <p>&copy; Diabetic Health Checker 2023</p>
+      </div>
+    </footer>
+  );
+}
+
 export function HealthStats() {
   const location = useLocation();
   const { bloodGlucose, systolicPressure, diastolicPressure, heartRate } = location.state || {};
@@ -10,7 +32,7 @@ export function HealthStats() {
   return (
     <div>
       <Navigation />
-      <h2>Health Stats</h2>
+      <Header />
       {/* received graph */}
       <BloodGlucoseChart
         bloodGlucose={bloodGlucose}
@@ -18,6 +40,7 @@ export function HealthStats() {
         diastolicPressure={diastolicPressure}
         heartRate={heartRate}
       />
+      <Footer />
     </div>
   );
 }
