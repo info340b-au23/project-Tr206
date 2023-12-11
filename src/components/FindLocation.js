73eaf8b8fec4f701a54d6e32a15931d77aa273d9
tr206 <tyler.merp@Tylers-MacBook-Air-106.local>
2023-12-11 13:32:09 -0800

    Updated Find Location on Zip and City

--- src/components/FindLocation.js
+++ src/components/FindLocation.js
@@ -3,97 +3,103 @@ import { Navigation } from './Navigation';
 import Papa from 'papaparse';
 
 export function FindLocation() {
-  const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [filterZipCode, setFilterZipCode] = useState('');
-  const [noHospitalsFound, setNoHospitalsFound] = useState(false);
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [filterInput, setFilterInput] = useState('');
+  const [noHospitalsFound, setNoHospitalsFound] = useState(false);
 
-  useEffect(() => {
-    // Load your CSV data here
-    const fetchData = async () => {
-      try { 
-        const response = await fetch('/data/Hospital_Data.csv');
-        const csvData = await response.text();
-        console.log('Fetched CSV Data:', csvData); // Log fetched CSV data for debugging
-        const parsedData = Papa.parse(csvData, { header: true }).data;
-        console.log('Parsed Data:', parsedData);
-        setData(parsedData);
-      } catch (error) {
-        console.error('Error loading CSV data:', error);
-      }
-    };
+  useEffect(() => {
+    // Load your CSV data here
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/data/Hospital_Data.csv');
+        const csvData = await response.text();
+        const parsedData = Papa.parse(csvData, { header: true }).data;
+        setData(parsedData);
+      } catch (error) {
+        console.error('Error loading CSV data:', error);
+      }
+    };
 
-    fetchData();
-  }, []); // Empty dependency array to ensure the effect runs only once
+    fetchData();
+  }, []); // Empty dependency array to ensure the effect runs only once
 
-  const findNearbyHospitals = () => {
-  if (!filterZipCode) {
-    setNoHospitalsFound(false);
-    setFilteredData([]);
-    return;
-  }
+  const findNearbyHospitals = () => {
+    if (!filterInput) {
+      setNoHospitalsFound(false);
+      setFilteredData([]);
+      return;
+    }
 
-  // Assuming your CSV has a 'ZIPCode' column
-  const hospitalsInZipCode = data.filter(
-    (hospital) => hospital.ZIPCode === filterZipCode.toLowerCase()
+    // Assuming your CSV has 'ZIPCode' and 'City' columns
+    const hospitalsMatchingInput = data.filter(
+      (hospital) =>
+        hospital.ZIPCode.toLowerCase() === filterInput.toLowerCase() ||
+        hospital.City.toLowerCase().includes(filterInput.toLowerCase())
+    );
+
+    if (hospitalsMatchingInput.length > 0) {
+      setNoHospitalsFound(false);
+      setFilteredData(hospitalsMatchingInput);
+    } else {
+      setNoHospitalsFound(true);
+      setFilteredData([]);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      findNearbyHospitals();
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <h1>Find Care Near You</h1>
+        </div>
+      </header>
+      <Navigation />
+      <div className="left-container">
+        <label htmlFor="filterInput">Enter ZIP code or City:</label>
+        <input
+          type="text"
+          id="filterInput"
+          value={filterInput}
+          onChange={(e) => setFilterInput(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+
+        <button onClick={findNearbyHospitals}>Find Nearby Hospitals</button>
+
+        <div id="tableContainer">
+          {noHospitalsFound ? (
+            <p>No hospitals found in your ZIP code or City.</p>
+          ) : filteredData.length === 0 ? (
+            <p>No nearby hospitals found.</p>
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  {Object.keys(filteredData[0]).map((key) => (
+                    <th key={key}>{key}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {filteredData.map((row, rowIndex) => (
+                  <tr key={rowIndex}>
+                    {Object.values(row).map((value, colIndex) => (
+                      <td key={colIndex}>{value}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </div>
   );
-  console.log('hospitalsInZipCode:', hospitalsInZipCode);
-
-  if (hospitalsInZipCode.length > 0) {
-    setNoHospitalsFound(false);
-    setFilteredData(hospitalsInZipCode);
-  } else {
-    setNoHospitalsFound(true);
-    setFilteredData([]);
-  }
-};
-
-  return (
-    <div>
-      <header>
-        <div className="container">
-          <h1>Find Care Near You</h1>
-        </div>
-      </header>
-      <Navigation />
-      <div className="left-container">
-        <label htmlFor="filterZipCode">Enter ZIP code:</label>
-        <input
-          type="text"
-          id="filterZipCode"
-          value={filterZipCode}
-          onChange={(e) => setFilterZipCode(e.target.value)}
-        />
-
-        <button onClick={findNearbyHospitals}>Find Nearby Hospitals</button>
-
-        <div id="tableContainer">
-          {noHospitalsFound ? (
-            <p>No hospitals found in your ZIP code.</p>
-          ) : filteredData.length === 0 ? (
-            <p>No nearby hospitals found.</p>
-          ) : (
-            <table>
-              <thead>
-                <tr>
-                  {Object.keys(filteredData[0]).map((key) => (
-                    <th key={key}>{key}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {filteredData.map((row, index) => (
-                  <tr key={index}>
-                    {Object.values(row).map((value, index) => (
-                      <td key={index}>{value}</td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>
-      </div>
-    </div>
-  );
 }
