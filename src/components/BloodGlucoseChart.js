2b45f767458d91c6d8265d5c021ad95e41ffa412
Morgan Weber <morgweb@uw.edu>
2023-12-11 12:00:22 -0800

    make changes to health stats page

--- src/components/BloodGlucoseChart.js
+++ src/components/BloodGlucoseChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   XYPlot,
   XAxis,
@@ -8,20 +8,25 @@ import {
   LineMarkSeries,
   FlexibleXYPlot,
 } from 'react-vis';
-import { onValue } from 'firebase/database';
+import { ref, onValue, push, set } from 'firebase/database';
 import { healthDataRef } from '../firebase/FirebaseConfig';
 
 const chartStyle = {
   marginBottom: '20px',
 };
 
-const BloodGlucoseChart = () => {
-  const [data, setData] = useState({
-    bloodGlucose: [], // Initialize as an empty array for each metric
+export const BloodGlucoseChart = () => {
+  const [chartData, setChartData] = useState({
+    bloodGlucose: [],
     systolicPressure: [],
     diastolicPressure: [],
     heartRate: [],
   });
+
+  const [bloodGlucose, setBloodGlucose] = useState('');
+  const [systolicPressure, setSystolicPressure] = useState('');
+  const [diastolicPressure, setDiastolicPressure] = useState('');
+  const [heartRate, setHeartRate] = useState('');
 
   const xMin = 0;
   const xMax = 24; // Assuming 24 hours
@@ -29,31 +34,91 @@ const BloodGlucoseChart = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const snapshot = await onValue(healthDataRef);
+        const snapshot = await onValue(healthDataRef); // healthDataRef needs to be defined or imported
         const healthData = snapshot.val();
 
-        // Assuming the data structure now has an array of entries with a timestamp and metrics
-        const updatedData = {
-          bloodGlucose: healthData.bloodGlucose || [], // Assuming it's an array of { timestamp, value }
-          systolicPressure: healthData.systolicPressure || [],
-          diastolicPressure: healthData.diastolicPressure || [],
-          heartRate: healthData.heartRate || [],
-        };
+        if (healthData) {
+          const updatedData = {
+            bloodGlucose: healthData.bloodGlucose || [],
+            systolicPressure: healthData.systolicPressure || [],
+            diastolicPressure: healthData.diastolicPressure || [],
+            heartRate: healthData.heartRate || [],
+          };
 
-        // Format the timestamp or handle it as needed for the x-axis
-        updatedData.bloodGlucose = updatedData.bloodGlucose.map(({ timestamp, value }) => ({
-          x: timestamp, // Adjust as needed for the x-axis value
-          y: value,
-        }));
-        // Similarly format other metrics if needed
+          updatedData.bloodGlucose = updatedData.bloodGlucose.map(({ timestamp, value }) => ({
+            x: timestamp,
+            y: value,
+          }));
 
-        setData(updatedData);
+          setChartData(updatedData);
+        }
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      // Update the state with the input values
+      await updateBloodGlucose(bloodGlucose);
+      await updateSystolicPressure(systolicPressure);
+      await updateDiastolicPressure(diastolicPressure);
+      await updateHeartRate(heartRate);
+  
+      // Clear the input fields after submission
+      setBloodGlucose('');
+      setSystolicPressure('');
+      setDiastolicPressure('');
+      setHeartRate('');
+    };
     fetchData();
-  }, []); // Run this effect only once on mount
+  }, []);
+
+
+  const updateBloodGlucose = async (newBloodGlucoseValue) => {
+    const dataRef = ref(healthDataRef.child('bloodGlucose'));
+    const newEntryRef = push(dataRef);
+    const timestamp = Date.now();
+
+    try {
+      await set(newEntryRef, { timestamp, value: newBloodGlucoseValue });
+    } catch (error) {
+      console.error('Error updating blood glucose data:', error);
+    }
+  };
+
+  const updateSystolicPressure = async (newSystolicPressureValue) => {
+    const dataRef = ref(healthDataRef.child('systolicPressure'));
+    const timestamp = Date.now();
+  
+    try {
+      await push(dataRef, { timestamp, value: newSystolicPressureValue });
+    } catch (error) {
+      console.error('Error updating systolic pressure data:', error);
+    }
+  };
+
+  const updateDiastolicPressure = async (newDiastolicPressureValue) => {
+    const dataRef = ref(healthDataRef.child('diastolicPressure'));
+    const timestamp = Date.now();
+  
+    try {
+      await push(dataRef, { timestamp, value: newDiastolicPressureValue });
+    } catch (error) {
+      console.error('Error updating diastolic pressure data:', error);
+    }
+  };
+
+  const updateHeartRate = async (newHeartRateValue) => {
+    const dataRef = ref(healthDataRef.child('heartRate'));
+    const timestamp = Date.now();
+  
+    try {
+      await push(dataRef, { timestamp, value: newHeartRateValue });
+    } catch (error) {
+      console.error('Error updating heart rate data:', error);
+    }
+  };
 
   return (
     <div>
@@ -62,66 +127,99 @@ const BloodGlucoseChart = () => {
         {/* Blood Glucose Chart */}
         <div style={chartStyle}>
           <h3 style={{ textAlign: 'center' }}>Blood Glucose Level</h3>
-          <FlexibleXYPlot
-            height={400}
-            width={600}
-            xType="linear"
-            xDomain={[xMin, xMax]}
-            yType="linear"
-            yDomain={[0, 35]}
-            margin={{ top: 40, right: 10, left: 40, bottom: 40 }}
-          >
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis title="Time (h)" />
-            <YAxis title="Blood Glucose (mmol/h)" />
-            <LineMarkSeries data={data.bloodGlucose} />
-          </FlexibleXYPlot>
+          {chartData.bloodGlucose.length > 0 ? (
+            <FlexibleXYPlot
+              height={400}
+              width={600}
+              xType="linear"
+              xDomain={[xMin, xMax]}
+              yType="linear"
+              yDomain={[0, 35]}
+              margin={{ top: 40, right: 10, left: 40, bottom: 40 }}
+            >
+              <VerticalGridLines />
+              <HorizontalGridLines />
+              <XAxis title="Time (h)" />
+              <YAxis title="Blood Glucose (mmol/h)" />
+              <LineMarkSeries data={chartData.bloodGlucose} />
+            </FlexibleXYPlot>
+          ) : (
+            <p>No blood glucose data available</p>
+          )}
         </div>
 
-        {/* Blood Pressure Chart */}
+        {/* Systolic Pressure Chart */}
         <div style={chartStyle}>
-          <h3 style={{ textAlign: 'center' }}>Blood Pressure</h3>
-          <FlexibleXYPlot
-            height={400}
-            width={600}
-            xType="linear"
-            xDomain={[xMin, xMax]}
-            yType="linear"
-            yDomain={[0, 200]}
-            margin={{ top: 40, right: 10, left: 40, bottom: 40 }}
-          >
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis title="Time (h)" />
-            <YAxis title="Blood Pressure (mmHg)" />
-            <LineMarkSeries data={data.systolicPressure} stroke="red" />
-            <LineMarkSeries data={data.diastolicPressure} stroke="blue" />
-          </FlexibleXYPlot>
+          <h3 style={{ textAlign: 'center' }}>Systolic Pressure</h3>
+          {chartData.systolicPressure.length > 0 ? (
+            <FlexibleXYPlot
+              height={400}
+              width={600}
+              xType="linear"
+              xDomain={[xMin, xMax]}
+              yType="linear"
+              yDomain={[0, 200]}
+              margin={{ top: 40, right: 10, left: 40, bottom: 40 }}
+            >
+              <VerticalGridLines />
+              <HorizontalGridLines />
+              <XAxis title="Time (h)" />
+              <YAxis title="Systolic Pressure (mmHg)" />
+              <LineMarkSeries data={chartData.systolicPressure} stroke="red" />
+            </FlexibleXYPlot>
+          ) : (
+            <p>No systolic pressure data available</p>
+          )}
         </div>
-        
+
+        {/* Diastolic Pressure Chart */}
+        <div style={chartStyle}>
+          <h3 style={{ textAlign: 'center' }}>Diastolic Pressure</h3>
+          {chartData.diastolicPressure.length > 0 ? (
+            <FlexibleXYPlot
+              height={400}
+              width={600}
+              xType="linear"
+              xDomain={[xMin, xMax]}
+              yType="linear"
+              yDomain={[0, 120]}
+              margin={{ top: 40, right: 10, left: 40, bottom: 40 }}
+            >
+              <VerticalGridLines />
+              <HorizontalGridLines />
+              <XAxis title="Time (h)" />
+              <YAxis title="Diastolic Pressure (mmHg)" />
+              <LineMarkSeries data={chartData.diastolicPressure} stroke="blue" />
+            </FlexibleXYPlot>
+          ) : (
+            <p>No diastolic pressure data available</p>
+          )}
+        </div>
+
         {/* Heart Rate Chart */}
         <div style={chartStyle}>
           <h3 style={{ textAlign: 'center' }}>Heart Rate</h3>
-          <FlexibleXYPlot
-            height={400}
-            width={600}
-            xType="linear"
-            xDomain={[xMin, xMax]}
-            yType="linear"
-            yDomain={[0, 140]}
-            margin={{ top: 40, right: 10, left: 40, bottom: 40 }}
-          >
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis title="Time (h)" />
-            <YAxis title="Heart Rate (BPM)" />
-            <LineMarkSeries data={data.heartRate} />
-          </FlexibleXYPlot>
+          {chartData.heartRate.length > 0 ? (
+            <FlexibleXYPlot
+              height={400}
+              width={600}
+              xType="linear"
+              xDomain={[xMin, xMax]}
+              yType="linear"
+              yDomain={[0, 140]}
+              margin={{ top: 40, right: 10, left: 40, bottom: 40 }}
+            >
+              <VerticalGridLines />
+              <HorizontalGridLines />
+              <XAxis title="Time (h)" />
+              <YAxis title="Heart Rate (BPM)" />
+              <LineMarkSeries data={chartData.heartRate} />
+            </FlexibleXYPlot>
+          ) : (
+            <p>No heart rate data available</p>
+          )}
         </div>
       </div>
     </div>
   );
 };
-
-export default BloodGlucoseChart;
--- src/components/HealthStats.js
+++ src/components/HealthStats.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Navigation } from './Navigation';
-import BloodGlucoseChart from './BloodGlucoseChart';
-import { healthDataRef } from '../firebase/FirebaseConfig';
-import { onValue, ref, push } from 'firebase/database';
+import { BloodGlucoseChart } from './BloodGlucoseChart';
 
 function Header() {
   return (
@@ -14,7 +12,7 @@ function Header() {
   );
 }
 
-function Footer() {
+function PageFooter() {
   return (
     <footer className="fixed-bottom">
       <div className="container">
@@ -27,53 +25,12 @@ function Footer() {
 }
 
 export function HealthStats() {
-  const [healthData, setHealthData] = useState([]);
-  const [bloodGlucose, setBloodGlucose] = useState('');
-  const [systolicPressure, setSystolicPressure] = useState('');
-  const [diastolicPressure, setDiastolicPressure] = useState('');
-  const [heartRate, setHeartRate] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const dataRef = ref(healthDataRef, 'healthData');
-    const newHealthData = {
-      bloodGlucose: parseInt(bloodGlucose),
-      systolicPressure: parseInt(systolicPressure),
-      diastolicPressure: parseInt(diastolicPressure),
-      heartRate: parseInt(heartRate),
-      timestamp: Date.now(),
-    };
-
-    // Push new health data to Firebase
-    push(dataRef, newHealthData);
-
-    // Update the chart or perform any other actions based on the new data
-    // You can choose to update the existing data or set it to a new state, similar to what was done in useEffect
-  };
-
   return (
     <div>
       <Navigation />
       <Header />
-
-      <form onSubmit={handleSubmit}>
-        {/* Input fields for capturing health data */}
-        <label>
-          Blood Glucose:
-          <input
-            type="number"
-            value={bloodGlucose}
-            onChange={(e) => setBloodGlucose(e.target.value)}
-          />
-        </label>
-        {/* Similar input fields for systolicPressure, diastolicPressure, and heartRate */}
-        <button type="submit">Submit</button>
-      </form>
-
-      {/* Display existing chart based on fetched healthData */}
-      <BloodGlucoseChart healthData={healthData} />
-      <Footer />
+      <BloodGlucoseChart />
+      <PageFooter />
     </div>
   );
 }
