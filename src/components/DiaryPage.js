d8c0499fa3cb71991e4490db1263de43d6faa8cb
Sanika Gadam <gadamsan@uw.edu>
2023-12-11 21:33:57 -0800

    Added media queries

--- src/components/DiaryPage.js
+++ src/components/DiaryPage.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ref, getDatabase, push as firebasePush, onValue } from 'firebase/database';
 import { Navigation } from './Navigation';
-import { firebaseApp } from '../firebase/FirebaseConfig';
-
+import { firebaseApp } from '../index';
 
 export function DiaryPage() {
   const [currentDate, setCurrentDate] = useState('');
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
+import { initializeApp } from 'firebase/app';
+import { ref, getDatabase } from 'firebase/database';
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAczDz776OKPO4GHBDue2GZ_CtIBeMKOt8",
+  authDomain: "diabetic-health-tracker.firebaseapp.com",
+  databaseURL: "https://diabetic-health-tracker-default-rtdb.firebaseio.com",
+  projectId: "diabetic-health-tracker",
+  storageBucket: "diabetic-health-tracker.appspot.com",
+  messagingSenderId: "877298090758",
+  appId: "1:877298090758:web:5cc58ca9c1f74e52601487",
+  measurementId: "G-6XFFM65BXR"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getDatabase(firebaseApp);
+
+
+const healthDataRef = ref(db, 'healthData')
+
+export { db, healthDataRef, firebaseApp };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
